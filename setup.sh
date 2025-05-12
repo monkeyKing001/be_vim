6eed822946f0a0daf22e56b0d8c1e56c4d156117
Dong Woo, Kwak <dongwoo.kwak@klleon.io>
2025-05-12 19:19:16 +0900

    fix: Add dependency nodejs.

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ if [[ "$OS" == "Darwin" ]]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
     echo "Installing necessary packages using brew..."
-    brew install python3 wget python3-pip python3-venv sudo clang llvm yarn
+    brew install python3 wget python3-pip python3-venv sudo clang llvm yarn nodejs npm
 
 # Install dependencies for Linux
 elif [[ "$OS" == "Linux" ]]; then
@@ -30,7 +30,7 @@ elif [[ "$OS" == "Linux" ]]; then
     fi
     echo "Installing necessary packages using apt-get..."
     apt-get update
-    apt-get install -y python3 python3-pip python3-venv sudo wget clang clangd yarn
+    apt-get install -y python3 python3-pip python3-venv sudo wget clang clangd yarn nodejs npm
 else
     echo "Unsupported operating system: $OS"
     exit 1
@@ -42,5 +42,8 @@ python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 chmod +x ./setup_vim.py
+
+sudo npm install -g n
+sudo n lts
 
 echo "Setup completed successfully."
