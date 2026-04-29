64e058d01d0f7d9050ae781937b67c7ca4b9d75f
monkeyKing001 <2018121152@yonsei.ac.kr>
2026-04-29 12:16:36 +0900

    chore: remove avante and retain render-markdown

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -59,43 +59,14 @@ require("lazy").setup({
     require("plugin_config.nerdtree")
   end
   },
-  -- Avante AI Assistant
+  -- Render Markdown
   {
-    "yetone/avante.nvim",
-    build = "make",
-    event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
-    config = function()
-        require("plugin_config.avante")()
-    end,
-    dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "stevearc/dressing.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "hrsh7th/nvim-cmp",
-    "ibhagwan/fzf-lua",
-    "nvim-tree/nvim-web-devicons",
-    {
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = { insert_mode = true },
-          use_absolute_path = true,
-        },
-      },
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    opts = {
+      file_types = { "markdown" },
     },
-    {
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-  },
+    ft = { "markdown" },
   },
   -- Clipboard image paste
   {
